137c26e8a3fd731ac1e6d34e868fe2932f66b81c
LunaLovecraft <lunalovecraft@gmail.com>
2016-02-23 15:44:02 -0500

    Added David's gitignore and the unity prototype

---
+++ GGPPrototype/Assets/LightInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightInput : MonoBehaviour {
+
+    float lightAngle;
+    float horz = 0;
+    float vert = 0;
+
+	// Update is called once per frame
+	void Update () {
+        horz = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+        if (horz != 0 || vert != 0)
+        {
+            this.transform.rotation = Quaternion.AngleAxis(-Mathf.Atan2(horz, vert) * Mathf.Rad2Deg + 90, new Vector3(0, 0, 1));
+        }
+
+	}
+}
---
+++ GGPPrototype/Assets/Ship.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+
+    Rigidbody2D myRigidbody;
+
+    void Start()
+    {
+        myRigidbody = this.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            myRigidbody.AddForce(new Vector2(-5.0f, 0.0f));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            myRigidbody.AddForce(new Vector2(5.0f, 0.0f));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            myRigidbody.AddForce(new Vector2(0.0f, 5.0f));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            myRigidbody.AddForce(new Vector2(0.0f, -5.0f));
+        }
+
+        if (myRigidbody.velocity.sqrMagnitude > 25.0f)
+        {
+            myRigidbody.velocity = myRigidbody.velocity / myRigidbody.velocity.magnitude * 5;
+        }
+
+        if (transform.position.x < -8.58f)
+        {
+            myRigidbody.MovePosition(new Vector2(transform.position.x + 2 * 8.58f, transform.position.y));
+            
+        }
+        else if (transform.position.x > 8.58f)
+        {
+            myRigidbody.MovePosition(new Vector2(transform.position.x - 2 * 8.58f, transform.position.y));
+        }
+
+        if (transform.position.y < -5)
+        {
+            myRigidbody.MovePosition(new Vector2(transform.position.x, transform.position.y + 10));
+        }
+        else if (transform.position.y > 5)
+        {
+            myRigidbody.MovePosition(new Vector2(transform.position.x, transform.position.y - 10));
+        }
+
+	}
+}
