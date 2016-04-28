69139085461995d6fa85e65cd17d235c425173d9
Gunny Waffle <davidafoxworthy@gmail.com>
2016-04-27 22:14:19 -0400

    shadowMap now included properly with new gitignore

---
+++ Engine/project/HSRC/Shaders/ShadowMap.hlsl
@@ -0,0 +1,37 @@
+struct SpotLight
+{
+	matrix view;
+	matrix projection;
+	float3 direction;
+	float fov;
+	float range;
+};
+
+cbuffer externalData : register(b0)
+{
+	matrix world;
+	SpotLight spotLight;
+};
+
+struct VertexShaderInput
+{
+	float3 position		: POSITION;
+	float3 normal		: NORMAL;
+	float3 tangent		: TANGENT;
+	float2 uv			: TEXCOORD;
+};
+
+struct VertexToPixel
+{
+	float4 position		: SV_POSITION;
+};
+
+VertexToPixel main(VertexShaderInput input)
+{
+	VertexToPixel output;
+
+	matrix wvp = mul(world, mul(spotLight.view, spotLight.projection));
+	output.position = mul(float4(input.position, 1.0f), wvp);
+
+	return output;
+}
