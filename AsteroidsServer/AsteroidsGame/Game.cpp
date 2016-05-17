af763d0d2417319a9e823d94d9c16211754ee20f
RossPerot <rmh8980@ad.rit.edu>
2016-05-16 21:39:42 -0400

    Revert "Asteroids are being drawn now"
    This reverts commit 57c6280d4dca7dbc900ef8e2b1ca0b9f611c0f63.

--- AsteroidsServer/AsteroidsGame/Game.cpp
+++ AsteroidsServer/AsteroidsGame/Game.cpp
@@ -43,27 +43,6 @@ Game::Game()
 		bulletsActive[i] = true;
 	}
 
-	for (int i = 0; i < MAX_ASTEROIDS; i++)
-	{
-		while (asteroidPositions->x[i] > -5 && asteroidPositions->x[i] < 5)
-		{
-			asteroidPositions->x[i] = rand() % 36 - 18;
-		}
-		while (asteroidPositions->y[i] > -5 && asteroidPositions->y[i] < 5)
-		{
-			asteroidPositions->y[i] = rand() % 20 - 10;
-		}
-		while (asteroidVelocities->x[i] == 0)
-		{
-			asteroidVelocities->x[i] = rand() % 6 - 3;
-		}
-		while (asteroidVelocities->y[i] == 0)
-		{
-			asteroidVelocities->y[i] = rand() % 6 - 3;
-		}
-
-	}
-
 	dirtyBuffers = false;
 }
 
@@ -109,7 +88,7 @@ void Game::Update(float deltaTime){
 	// A few physics constants
 	__m128 dt = _mm_set1_ps(deltaTime);
 
-	//printf("Asteroid X Position: %f \n", asteroidPositions->x[0]);
+	//printf("Ship X Position: %f \n", shipPositions->x[0]);
 
 	//
 	// Asteroid Physics
